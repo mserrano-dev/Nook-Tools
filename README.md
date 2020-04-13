2bc7b4120e21129c6cdfbc582e4270a9e99c12ce
Mark Anthony Serrano <markanthony.serrano2@gmail.com>
2020-04-13 16:50:33 -0700

    Added a set of tools to facilitate Shopify development with ThemeKit and version control. Included is a .env file for variables such as ThemeKit creds and git author info.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Nook-Tools
+# Shopify Starter Theme
+
+##### define a .env file (see .env.default)
+> $ yarn install
+
+| Command      | Result                                                           |
+| :----------- | :----------------------------------------------------------------|
+| $ yarn git   | will add your creds to .git/config, as defined by .env variables |
+| $ yarn sync  | will download the hosted shopify theme into the local env        |
+| $ yarn dev   | will watch for changes in the src folder and upload them         |
+| $ yarn build | will minimize the bundled files and zip the repo                 |
---
+++ devops.js
@@ -0,0 +1,87 @@
+const shell = require('shelljs');
+const argv = require('yargs').argv
+//
+const themekit_config = get_auth_flags();
+
+switch (argv.mode) {
+  case 'git':
+    // setup git author in the .git/config file
+    if (valid_env_variables("GIT_NAME", "GIT_USER", "GIT_EMAIL") === true) {
+      shell.exec(chain_cmd(
+        `git config user.name "${ process.env.GIT_NAME }"`,
+        `git config credential.username "${ process.env.GIT_USER }"`,
+        `git config user.email "${ process.env.GIT_EMAIL }"`,
+        `echo SUCCESS`
+      ));
+    } else {
+      shell.exec('echo FAILED');
+    }
+    break;
+  case 'sync':
+    // pull what is deployed into the local environment
+    if(themekit_config) {
+      shell.exec(chain_cmd(
+        `theme get ${ themekit_config } --dir src`,
+      ));
+    }
+    break;
+  case 'dev':
+    // start the dev watch process
+    if(themekit_config) {
+      shell.exec(chain_cmd(
+        `theme watch ${ themekit_config } --dir src`,
+      ));
+    }
+    break;
+  case 'build':
+    // build a minified version and zip up the repo
+    if(themekit_config) {
+      shell.exec(chain_cmd(
+        `theme get ${ themekit_config } --dir src`,
+      ));
+    }
+    break;
+}
+
+// -- Functions --
+function chain_cmd(...args) {
+  return args.join(';');
+}
+
+function get_auth_flags() {
+  let _return = false;
+
+  // return the loaded auth params
+  if(valid_env_variables("SHOPIFY_PASSWD", "SHOPIFY_THEMEID", "SHOPIFY_SHOP") === true) {
+    const password = ` --password ${ process.env.SHOPIFY_PASSWD }`;
+    const themeid = ` --themeid ${ process.env.SHOPIFY_THEMEID }`;
+    const store = ` --store ${ process.env.SHOPIFY_SHOP }`;
+
+    _return = password + themeid + store;
+  }
+  return _return;
+}
+
+function valid_env_variables(...keys) {
+  // validate the .env file, reporting any error
+  let _return = true;
+  for(var i in keys) {
+    var is_valid = valid_env_variable(keys[i]);
+    _return = _return && is_valid;
+  }
+  return _return;
+}
+
+function valid_env_variable(key) {
+  let val = process.env[key];
+  let _return = true;
+  
+  if(typeof val === 'undefined') {
+    _return = false; // empty value
+    console.error(`ERROR: missing "${ key }" in the .env file`)
+  } else if((val.indexOf('"') !== -1) || (val.indexOf("'") !== -1)) {
+    _return = false; // malformatted
+    console.error(`ERROR: malformatted "${ key }" in the .env file`)
+  }
+  return _return;
+}
