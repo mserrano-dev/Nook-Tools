3854c96b624f1c368cd0f73a3716aa8dbd5c91f0
Mark Anthony Serrano <markanthony.serrano2@gmail.com>
2020-04-15 22:13:55 -0700

    Add ability to use globs in Sass imports. Sass source maps fix.

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
 const { watch, src, dest, parallel } = require('gulp');
-const shell = require('shelljs');
 const concat = require('gulp-concat');
 const rename = require('gulp-rename');
 const touch = require('gulp-touch-fd');
 const sass = require('gulp-sass');
+const globImporter = require('node-sass-glob-importer');
 const cssnano = require('gulp-cssnano');
 const sourcemaps = require('gulp-sourcemaps');
 const gulpif = require('gulp-if');
 const uglify = require('gulp-uglify');
-const argv = require('yargs').argv
+const argv = require('yargs').argv;
 //
 const env = {
   production: (argv.env == 'production'),
   development: (argv.env == 'development'),
-}
+};
 let project = null;
 update_project_vars();
 
 function update_project_vars() {
-  delete require.cache[require.resolve('./gulpfile-vars')]
+  delete require.cache[require.resolve('./gulpfile-vars')];
   project = require('./gulpfile-vars');
 }
 
@@ -26,14 +26,19 @@ function css() {
   update_project_vars();
   return src(project.styles.sass_entry)
     .pipe(gulpif(env.development, sourcemaps.init()))
-    .pipe(sass()).on('error', sass.logError)
-    .pipe(gulpif(env.development, sourcemaps.write('.')))
-    .pipe(gulpif(env.production, cssnano()))
-    .pipe(rename(project.styles.filename))
+      .pipe(sass({
+        importer: globImporter()
+      })).on('error', sass.logError)
+      .pipe(cssnano())
+      .pipe(rename(project.styles.filename))
+    .pipe(gulpif(env.development, sourcemaps.write()))
     .pipe(dest(project.asset_folder))
-    .pipe(touch()); /* 
+    .pipe(touch()) /* 
       Gulp4 does not update mtime so we do it manually with touch.
       ThemeKit will upload once file mtime is updated.
+
+      https://github.com/gulpjs/gulp/issues/2193
+      https://github.com/Shopify/themekit/issues/607
     */
 }
 
